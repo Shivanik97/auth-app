23916e5e7be901c8c17901a13627b9492440099d
Shivani Kuntumalla <skuntumalla@ariqt.com>
2023-03-01 14:50:57 +0530

    auth-app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import quasarUserOptions from './quasar-user-options'
+import { Quasar } from 'quasar'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(Quasar, quasarUserOptions).use(router).mount('#app')
